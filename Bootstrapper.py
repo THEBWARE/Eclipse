6794b01d3a92a89b22d69c6691398c7805ac79d0
THEB <Worldroleplayman@gmail.com>
2025-05-11 15:33:43 -0400

    Update Bootstrapper.py

--- Bootstrapper.py
+++ Bootstrapper.py
@@ -1,34 +1,29 @@
 import requests
 import zipfile
 import io
-from colorama import init, Fore
 
-init()
-
-eclipse_art = f"""
-{Fore.BLUE}
+eclipse_art = """
  _____     _ _                
 | ____|___| (_)_ __  ___  ___ 
 |  _| / __| | | '_ \/ __|/ _ \
 | |__| (__| | | |_) \__ \  __/
 |_____\___|_|_| .__/|___/\___|
-              |_|             
-{Fore.RESET}
+              |_|              
 """
 
 def download_and_extract(url, extract_to='.'):
-    print(f"{Fore.BLUE}\nDownloading file from {url}...{Fore.RESET}")
+    print("\nDownloading file from {}...".format(url))
     try:
         response = requests.get(url, stream=True)
         response.raise_for_status()
         
-        print(f"{Fore.BLUE}Extracting files...{Fore.RESET}")
+        print("Extracting files...")
         with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
             zip_ref.extractall(extract_to)
         
-        print(f"{Fore.BLUE}Download and extraction completed successfully!{Fore.RESET}")
+        print("Download and extraction completed successfully!")
     except Exception as e:
-        print(f"{Fore.RED}An error occurred: {e}{Fore.RESET}")
+        print("An error occurred:", e)
 
 if __name__ == "__main__":
     print(eclipse_art)
